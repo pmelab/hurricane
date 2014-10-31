5dc6713f5ec4dffcd0a25da287b601d99fe61afe
pmelab <philipp.melab@gmail.com>
2014-10-31 11:53:14 +0100

    Issue #2366421 by tanc, pmelab: Fixed black squares instead of throbbers on ajax buttons.

--- hurricane_integration/hurricane_ajax.js
+++ hurricane_integration/hurricane_ajax.js
@@ -11,24 +11,18 @@
   var hurricane_beforesend = Drupal.ajax.prototype.beforeSend;
   Drupal.ajax.prototype.beforeSend = function (xmlhttprequest, options) {
     hurricane_beforesend.apply(this, [xmlhttprequest, options]);
-    var $wrapper = $(this.element).parents('.form-item').first();
-    $wrapper.addClass('form-ajax-progress');
-    $wrapper.find('.throbber').hurricane('start');
+    $(this.element).parent().find('.throbber').hurricane('start');
   };
 
   // Remove ".form-ajax-progress" from the wrapping elements
   // on success or error.
   var hurricane_success = Drupal.ajax.prototype.success;
   Drupal.ajax.prototype.success = function(response, status) {
-    var $wrapper = $(this.element).parents('.form-item').first();
-    $wrapper.removeClass('form-ajax-progress');
     hurricane_success.apply(this, [response, status]);
   };
 
   var hurricane_error = Drupal.ajax.prototype.error;
   Drupal.ajax.prototype.error = function(response, uri) {
-    var $wrapper = $(this.element).parents('.form-item').first();
-    $wrapper.removeClass('form-ajax-progress');
     hurricane_error.apply(this, [response, uri]);
   };
 }(jQuery));
